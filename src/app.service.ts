64aef1bada73046f363f095a5a9d78f29882d0e1
levi-atan <162452300+levi-atan@users.noreply.github.com>
2024-03-07 17:50:10 +0000

    semmit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param, Query } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getPersonalizedHello(name: string, day: string): string {
+    return 'Hello ${name}, have a nice ${day}';
+  }
+
+  @Get('/hello/:name')
+  buzi(@Param('name') name: string, @Query('day') day: string) {
+    return this.appService.getPersonalizedHello(name, day);
   }
 }
